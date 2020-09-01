9571444202a98088fc37f0d982c8150930f9be94
比嘉助 <s17009@std.it-college.ac.jp>
2020-09-01 17:51:06 +0900

    サブビューの読み込み

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -19,15 +19,7 @@
   <p>ここが本文のコンテンツです</p>
   <p>必要なだけ記述できます。</p>
   
-  @component('components.message')
-    @slot('msg_title')
-    CAUTION!
-    @endslot
-
-    @slot('msg_content')
-    これはメッセージの表示ー
-    @endslot
-  @endcomponent
+  @include('components.message', ['msg_title' => 'OK', 'msg_content' => 'サブビュー'])
 @endsection
 
 @section('footer')
