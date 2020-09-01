56daafe5cdae4386c2551db42db08f4d1a562a7c
比嘉助 <s17009@std.it-college.ac.jp>
2020-09-01 20:12:43 +0900

    サービスプロバイダ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,12 +9,7 @@ class HelloController extends Controller
 {
   public function index()
   {
-    $data = [
-      ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
-      ['name'=>'田中はなこ', 'mail'=>'hanako@hoge'],
-      ['name'=>'鈴木さちこ', 'mail'=>'suzuki@huga']
-    ];
-    return view('hello.index', ['data'=>$data]);
+    return view('hello.index', ['message'=>'Hello!']);
   }
 
   
