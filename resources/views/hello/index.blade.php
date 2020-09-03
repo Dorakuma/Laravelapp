518adfd124b343775e6fcd8e03451bc9ff6e8329
比嘉助 <s17009@std.it-college.ac.jp>
2020-09-03 15:08:01 +0900

    バリデーションの基本処理を作ったー

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -16,9 +16,16 @@
 @endsection
 
 @section('content')
-  <p>ここが本文のコンテンツです</p>
-  <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-  <p>これは、<middleware>yahoo.com</middleware>へのリンクです。</p>
+  <p>{{$msg}}</p>
+  <form action="/hello" method="post">
+  <table>
+    @csrf
+    <tr><th>name: </th><td><input type="text" name="name"></td></tr>
+    <tr><th>mail: <td><input type="text" name="mail"></td></th></tr>
+    <tr><th>age: <td><input type="text" name="age"></td></th></tr>
+    <tr><th></th><td><input type="submit" value="send"></td></tr>
+  </table>
+  </form>
 @endsection
 
 @section('footer')
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index')->middleware('helo');
+Route::get('hello', 'HelloController@index');
 
 Route::post('hello', 'HelloController@post');
