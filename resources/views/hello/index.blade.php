17574f3734fe8b1676d84dc433f32073d95bbc16
比嘉助 <s17009@std.it-college.ac.jp>
2020-09-04 11:59:10 +0900

    バリデーションのエラー出力できるようにしたー

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -17,12 +17,21 @@
 
 @section('content')
   <p>{{$msg}}</p>
+  @if (count($errors) > 0)
+    <div>
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
   <form action="/hello" method="post">
   <table>
     @csrf
-    <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-    <tr><th>mail: <td><input type="text" name="mail"></td></th></tr>
-    <tr><th>age: <td><input type="text" name="age"></td></th></tr>
+    <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+    <tr><th>mail: <td><input type="text" name="mail" value="{{old('mail')}}"></td></th></tr>
+    <tr><th>age: <td><input type="text" name="age" value="{{old('age')}}"></td></th></tr>
     <tr><th></th><td><input type="submit" value="send"></td></tr>
   </table>
   </form>
