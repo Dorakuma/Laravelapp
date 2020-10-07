9a2640339fbc7f036b1db784c6c3b5e6cf2ab94c
Dorakuma <tasukehiga@gmail.com>
2020-10-07 21:49:48 +0900

    Authのログインと新規作成画面の自動生成すげぇ...

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -30,4 +30,9 @@
     @endforeach
   </table>
   {{$items->appends(['sort' => $sort])->links()}}
+  @if (Auth::check())
+    <p>USER: {{$user->name . ' (' . $user->email . ')'}}</p>
+  @else
+    <p>※ログインしていません。(<a href="/login">ログイン</a> | <a href="/register">登録</a>)</p>
+  @endif
 @endsection
